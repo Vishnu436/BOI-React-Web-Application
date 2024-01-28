bbf2337aca524a94958b7ac799e37f8015022701
kamarsu96 <krt9296@gmail.com>
2024-01-28 12:17:25 +0530

    Committed by saikumar on 28/01/24

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Login from "./pages/Login.jsx";
 import Layout from "./pages/Layout.jsx";
 import Applications from "./Mainmenu/Applications.jsx";
 import Dashboard from "./Mainmenu/Dashboard.jsx";
+import HttpsRedirect from 'react-https-redirect';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/main" element={<Layout />}>
-          <Route path="/main/applications" element={<Applications />} />
-          <Route path="/main/dashboard" element={<Dashboard />} /> 
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <HttpsRedirect>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/main" element={<Layout />}>
+            <Route path="/main/applications" element={<Applications />} />
+            <Route path="/main/dashboard" element={<Dashboard />} /> 
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </HttpsRedirect>
   )
 }
 
